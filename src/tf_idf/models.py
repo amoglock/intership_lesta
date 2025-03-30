7e30b2cb803449b22c503b6446364acaca6d5cd3
AmOglock <amoglock@gmail.com>
2025-03-31 01:55:42 +0300

    Removed the WordResult class from models.py since it is no longer in use. Only the ResultModel class is left for working with TF-IDF analysis results.

--- src/tf_idf/models.py
+++ src/tf_idf/models.py
@@ -4,11 +4,6 @@ from pydantic import BaseModel
 
 from src.models import Analysis
 
-class WordResult(BaseModel):
-    word: str
-    tf: float
-    idf: float
-
 class ResultModel(SQLModel):
     analysis: Analysis
     results: List[Tuple[str, float, float]]
