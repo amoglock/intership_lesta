c0c27703f45885b87280cabfdf026280d05184ef
AmOglock <amoglock@gmail.com>
2025-06-08 02:06:15 +0300

    Add CollectionsService class for managing collections and documents

---
+++ src/collections/service.py
@@ -0,0 +1,35 @@
+from src.collections.repository import CollectionsRepository
+from src.collections.schemas import CollectionResponse
+from src.documents.repository import DocumentsRepository
+
+
+class CollectionsService:
+
+    def __init__(self):
+        self.repository = CollectionsRepository()
+        self.documents_repository = DocumentsRepository()
+
+    async def add_document_to_collection(self, collection_id: int, document_id: int) -> None:
+        """ """
+        collection = await self.repository.get_collection_by_id(collection_id)
+        document = await self.documents_repository.get_one_or_all_documents(document_id)
+        await self.repository.add_document_to_collection(collection, document)
+
+    async def create_collection(self):
+        """ """
+        return await self.repository.create_collection()
+
+    async def collection(self, collection_id: int) -> list:
+        """ """
+        collection = await self.repository.get_collection_by_id(collection_id)
+        return collection.documents
+
+    async def collections(self) -> list[CollectionResponse]:
+        """ """
+        return await self.repository.get_all_collections()
+    
+    async def delete_document(self, collection_id: int, document_id: int) -> None:
+        """ """
+        collection = await self.repository.get_collection_by_id(collection_id)
+        document = await self.documents_repository.get_one_or_all_documents(document_id)
+        await self.repository.remove_document_from_collection(collection, document)
