19bfc2a8f477ec945c3c50a83b7126d1c4f4bdf8
AmOglock <amoglock@gmail.com>
2025-06-08 17:19:07 +0300

    Add ResultModel schema for TF-IDF results

---
+++ src/tf_idf/schemas.py
@@ -0,0 +1,10 @@
+from typing import List, Tuple
+from sqlmodel import SQLModel
+
+
+# from src.models import Analysis
+
+class ResultModel(SQLModel):
+    # analysis: Analysis
+    results: List[Tuple[str, float, float]]
+    
