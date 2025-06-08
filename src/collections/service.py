dcd1512bc99d6d3b9e35c33f5d821d4c9766a9e5
AmOglock <amoglock@gmail.com>
2025-06-08 17:16:41 +0300

    Add DocumentsService and TFIDFProcessor to CollectionsService; implement get_collection_statistics method

--- src/collections/service.py
+++ src/collections/service.py
@@ -1,6 +1,8 @@
 from src.collections.repository import CollectionsRepository
 from src.collections.schemas import CollectionResponse
 from src.documents.repository import DocumentsRepository
+from src.documents.service import DocumentsService
+from src.tf_idf.processor import TFIDFProcessor
 
 
 class CollectionsService:
@@ -8,8 +10,12 @@ class CollectionsService:
     def __init__(self):
         self.repository = CollectionsRepository()
         self.documents_repository = DocumentsRepository()
+        self.documents_sevice = DocumentsService()
+        self.processor = TFIDFProcessor()
 
-    async def add_document_to_collection(self, collection_id: int, document_id: int) -> None:
+    async def add_document_to_collection(
+        self, collection_id: int, document_id: int
+    ) -> None:
         """ """
         collection = await self.repository.get_collection_by_id(collection_id)
         document = await self.documents_repository.get_one_or_all_documents(document_id)
@@ -27,9 +33,21 @@ class CollectionsService:
     async def collections(self) -> list[CollectionResponse]:
         """ """
         return await self.repository.get_all_collections()
-    
+
     async def delete_document(self, collection_id: int, document_id: int) -> None:
         """ """
         collection = await self.repository.get_collection_by_id(collection_id)
         document = await self.documents_repository.get_one_or_all_documents(document_id)
         await self.repository.remove_document_from_collection(collection, document)
+
+    async def get_collection_statistics(self, collection_id):
+        """ """
+        collection_texts = []
+        collection = await self.repository.get_collection_by_id(
+            collection_id=collection_id
+        )
+        for doc in collection.documents:
+                content = await self.documents_sevice.get_file_content(doc.file_path)
+                collection_texts.append(content)
+
+        return await self.processor.collection_statictics(collection_texts)
