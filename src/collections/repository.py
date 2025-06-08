b5057716d13416f01efbac23467ad841db808de6
AmOglock <amoglock@gmail.com>
2025-06-08 17:39:37 +0300

    Add docstrings to CollectionsRepository methods for better documentation

--- src/collections/repository.py
+++ src/collections/repository.py
@@ -6,6 +6,11 @@ from src.models import Collection, Document, CollectionDocumentLink
 
 
 class CollectionsRepository:
+    """Repository for managing collections and their documents.
+    
+    This repository handles all database operations related to collections,
+    including CRUD operations and managing document-collection relationships.
+    """
 
     def __init__(self):
         self.engine = engine
@@ -13,14 +18,23 @@ class CollectionsRepository:
     async def add_document_to_collection(
         self, collection: Collection, document: Document
     ) -> None:
-        """ """
+        """Add a document to a collection.
+        
+        Args:
+            collection: Collection to add document to
+            document: Document to add to collection
+        """
         with Session(self.engine) as session:
             collection.documents.append(document)
             session.add(collection)
             session.commit()
 
     async def create_collection(self) -> Collection:
-        """ """
+        """Create a new collection.
+        
+        Returns:
+            Collection: Created collection
+        """
         with Session(self.engine) as session:
             collection = Collection(name="My collection")
             session.add(collection)
@@ -29,14 +43,29 @@ class CollectionsRepository:
             return collection
         
     async def remove_document_from_collection(self, collection: Collection, document: Document):
-        """ """
+        """Remove a document from a collection.
+        
+        Args:
+            collection: Collection to remove document from
+            document: Document to remove from collection
+        """
         with Session(self.engine) as session:
             collection.documents.remove(document)
             session.add(collection)
             session.commit()
 
     async def get_collection_by_id(self, collection_id: int) -> Collection:
-        """ """
+        """Get a collection by its ID.
+        
+        Args:
+            collection_id: ID of the collection to get
+            
+        Returns:
+            Collection: Collection with its documents
+            
+        Raises:
+            NoResultFound: If collection with given ID doesn't exist
+        """
         with Session(self.engine) as session:
             template = (
                 select(Collection)
@@ -48,12 +77,11 @@ class CollectionsRepository:
 
     async def get_all_collections(self) -> list[CollectionResponse]:
         """Get all collections with their documents.
-
+        
         Returns:
-            list[Collection]: List of all collections with their documents
+            list[CollectionResponse]: List of all collections with their documents
         """
         with Session(self.engine) as session:
             template = select(Collection).options(selectinload(Collection.documents))
             collections = session.exec(template).all()
-
             return collections
