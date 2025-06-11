3f7c442960c864ef83865b68b07ead13c8ec9e27
AmOglock <amoglock@gmail.com>
2025-06-11 19:17:29 +0300

    Add file validation function for uploaded files

---
+++ src/documents/dependencies.py
@@ -0,0 +1,38 @@
+import os
+from fastapi import UploadFile
+
+from src.core.config import settings
+from src.documents.exceptions import (
+    FileExtensionValidationError,
+    FileSizeValidationError,
+    FileTypeValidationError,
+)
+
+
+async def file_validation(file: UploadFile) -> UploadFile:
+    """Validates file size, content type and file extension.
+
+    Args:
+        file (UploadFile): Uploaded file by form
+
+    Returns:
+        bool: True if validation is successful, otherwise False
+
+    Note:
+        Validates against:
+        - settings.MAX_FILE_SIZE: Maximum allowed file size
+        - settings.ALLOWED_FILE_TYPES: List of allowed MIME types
+        - settings.ALLOWED_EXTENSIONS: List of allowed file extensions
+    """
+    ext = os.path.splitext(file.filename)[1].lower()
+
+    if ext not in settings.ALLOWED_EXTENSIONS:
+        raise FileExtensionValidationError()
+
+    if file.size > settings.MAX_FILE_SIZE:
+        raise FileSizeValidationError()
+
+    if file.content_type not in settings.ALLOWED_FILE_TYPES:
+        raise FileTypeValidationError()
+
+    return file
